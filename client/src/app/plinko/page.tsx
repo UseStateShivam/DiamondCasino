d5e0a933919c0e1406b1e1cf468a9df2f717eb98
OptionsBuffetDev <mcrafter147@gmail.com>
2024-07-15 21:27:33 +0530

    s

--- client/src/app/plinko/page.tsx
+++ client/src/app/plinko/page.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
+'use client'
 
-function Plinko() {
+import { useEffect, useRef, useState } from "react";
+import { BallManager } from "@/games/plinko/classes/BallManager";
+import axios from "axios";
+
+const Game = () => {
+  const [ballManager, setBallManager] = useState<BallManager>();
+  const canvasRef = useRef<any>();
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ballManager = new BallManager(
+        canvasRef.current as unknown as HTMLCanvasElement
+      );
+      setBallManager(ballManager);
+    }
+  }, [canvasRef]);
+
   return (
-    <>
-      <div>
-        <h1>Welcome to Plinko!</h1>
-      </div>
-    </>
-  )
+    <div className="flex flex-col lg:flex-row items-center justify-center">
+      <canvas ref={canvasRef} width="800" height="800"></canvas>
+      <button
+        className="px-10 mb-4"
+        onClick={async () => {
+          const response = await axios.post(`http://localhost:8800/game`, {
+            data: 1,
+          });
+          if (ballManager) {
+            ballManager.addBall(response.data.point);
+          }
+        }}
+      >
+        Add ball
+      </button>
+    </div>
+  );
 }
 
-export default Plinko
+export default Game;
--- client/src/games/plinko/constants.ts
+++ client/src/games/plinko/constants.ts
@@ -5,7 +5,7 @@ export const WIDTH = 800
 export const obstacleRadius = 4
 export const ballRadius = 7
 
-export const gravity: number = pad(0.6)
+export const gravity: number = pad(0.1)
 export const horizontalFriction: number = 0.4
 export const verticalFriction: number = 0.6
 
